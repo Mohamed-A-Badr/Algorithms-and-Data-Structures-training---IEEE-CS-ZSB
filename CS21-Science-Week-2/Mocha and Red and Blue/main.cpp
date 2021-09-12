2a0d957f4ac4e8934f201562bfb449178ad41a59
Mohamed Badr <60277974+Mohamed-A-Badr@users.noreply.github.com>
2021-09-13 00:44:20 +0200

    Create main.cpp

---
+++ CS21-Science-Week-2/Mocha and Red and Blue/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long int ll;
+#define fast_ios ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define file_in freopen("input.txt", "r", stdin)
+#define el "\n"
+#define pb push_back
+#define E 2.768
+#define MAX 1e5
+
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int en = n - 1;
+    while(s[en] == '?'){
+        en--;
+    }
+    for(int i = en + 1; i < n; i++){
+        if(s[i] == '?'){
+            if(s[i - 1] == 'R')
+                s[i] = 'B';
+            else
+                s[i] = 'R';
+        }
+    }
+    for(int i = en - 1; i >= 0; i--){
+        if(s[i] == '?'){
+            if(s[i + 1] == 'B')
+                s[i] = 'R';
+            else
+                s[i] = 'B';
+        }
+    }
+    cout << s << el;
+}
+
+int main()
+{
+    fast_ios;
+    //file_in;
+    int t; cin >> t;while(t--){solve();}
+    //solve();
+    return 0;
+}
